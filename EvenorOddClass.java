23dd4bd9465841dd80441d551067c0bffc218cdb
shruti patel <Shrutipatel203@gmail.com>
2023-10-31 19:30:20 +0000

    evenoroddcommit

---
+++ EvenorOddClass.java
@@ -0,0 +1,15 @@
+package EvenorOdd;
+
+public class EvenorOddClass {
+    public static void main(String[] args){
+        int num=13;
+        if(num %2 == 0){
+            System.out.println("number is even");
+        }
+        else {
+            System.out.println("number is odd");
+
+        }
+
+    }
+}
